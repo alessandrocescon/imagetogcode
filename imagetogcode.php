83e3da08c0c004b61e555d48b4734c7822131bd2
alessandrocescon <info@eyefly.it>
2018-01-26 14:48:21 +0100

    Added GD function as default and imagick optional

--- imagetogcode.php
+++ imagetogcode.php
@@ -1,9 +1,9 @@
 <?php
 
 //params
-$gcodefilename="bn.gcode";
-$imagefilename="bn.png";
-$grayscaledimage="bngray.jpg";
+$gcodefilename="bike.gcode";
+$imagefilename="bike.jpg";
+$grayscaledimage="bikegray.jpg";
 $thickness = 2.50;
 $workspeed = 50;
 $movespeed = 150;
@@ -21,25 +21,32 @@ $finh = intval(floatval($prodheight)/floatval($tooldiam));
 $gcodefile = fopen($gcodefilename, "w");
 
 //open image
-$im = new Imagick( $imagefilename );
+//$im = new Imagick( $imagefilename );
+$im = imagecreatefromjpeg($imagefilename);
 
 //grayscale image
-$im->modulateImage(100,0,100);
+//$im->modulateImage(100,0,100);
+imagefilter($im, IMG_FILTER_GRAYSCALE);
 
 //scale image for passes http://php.net/manual/en/imagick.resizeimage.php
-$im->resizeImage($finw,$finh,Imagick::FILTER_LANCZOS,0);
+//$im->resizeImage($finw,$finh,Imagick::FILTER_LANCZOS,0);
+$im = imagescale($im, $finw, $finh, IMG_BILINEAR_FIXED);
 
 //save scaled image for check
-$im->setImageFormat ("jpeg");
-file_put_contents($grayscaledimage, $im);
+//$im->setImageFormat ("jpeg");
+//file_put_contents($grayscaledimage, $im);
+imagejpeg($im, $grayscaledimage);
 
 //start gcode file
 fwrite($gcodefile, "%\n");
 fwrite($gcodefile, "M3 S".$spindle."\nG21\nG00 Z".$safez." F".$movespeed."\n");
 
 //get final w / h
-$imw=$im->getImageWidth();
-$imh=$im->getImageHeight();
+//$imw=$im->getImageWidth();
+//$imh=$im->getImageHeight();
+
+$imw = imagesx($im);
+$imh = imagesy($im);
 
 //loop rows
 for ($x = 0; $x < $imw; $x++) {
@@ -51,9 +58,12 @@ for ($x = 0; $x < $imw; $x++) {
           $ry=$imh-$y-1;
            $yc=$y;
        }
-       $pixel = $im->getImagePixelColor($x,$yc);
-       $color = $pixel->getColor();
-       $zval = (floatval(floatval($thickness)/floatval(255)))*(floatval($color['r']));
+       //$pixel = $im->getImagePixelColor($x,$yc);
+       //$color = $pixel->getColor();
+       //$zval = (floatval(floatval($thickness)/floatval(255)))*(floatval($color['r']));
+       $pixel = imagecolorat($im,$x,$yc);
+       $rval= ($pixel >> 16) & 0xFF;
+       $zval = (floatval(floatval($thickness)/floatval(255)))*(floatval($rval));
        $zval = number_format($zval, 2, '.', '');
        fwrite($gcodefile, "G01 Z-".$zval." F".$workspeed."\n");
        $xg=number_format(floatval($x*$tooldiam), 2, '.', '');
@@ -67,6 +77,6 @@ fwrite($gcodefile, "G00 Z".$safez." F".$movespeed."\nM5\nG00 X0.00 Y0.00 F".$mov
 fwrite($gcodefile, "%\n");
 
 //close gcode file
-$im->clear();
+//$im->clear();
 fclose($gcodefile);
 ?>
